ea2ebdbc2b9ddeb3aaf15b422c087d69b0aef8a3
Carlos Abarca Calderon <extcabarca@transbank.cl>
2024-11-24 19:39:21 -0300

    Recibir imagenes para subirlas a cloudinary

--- src/actions/products/create-update-product.action.ts
+++ src/actions/products/create-update-product.action.ts
@@ -5,6 +5,15 @@ import { z } from 'zod';
 import { v4 as UUID } from 'uuid';
 import { db, eq, Product } from 'astro:db';
 
+const MAX_FILE_SIZE = 5_000_000; // 5MB
+const ACCEPTED_IMAGE_TYPES = [
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'image/webp',
+    'image/svg+xml',
+];
+
 export const createUpdateProduct = defineAction({
     accept: 'form',
     input: z.object({
@@ -18,8 +27,16 @@ export const createUpdateProduct = defineAction({
         tags: z.string(),
         title: z.string(),
         type: z.string(),
-    
-        //TODO Imagen
+
+        imageFiles: z.array(
+            z.instanceof(File)
+                .refine(file => file.size <= MAX_FILE_SIZE, 'Max image size 5MB')
+                .refine(file => {
+                    if( file.size === 0 ) return true;
+                    
+                    return ACCEPTED_IMAGE_TYPES.includes(file.type);
+                }, `Only supported image files are valid, ${ACCEPTED_IMAGE_TYPES.join(',')}` )
+        ).optional(),
     }),
     handler: async (form, { request }) => {
 
@@ -27,12 +44,12 @@ export const createUpdateProduct = defineAction({
         const user = session?.user;
 
         //Podemos validar si el usuario tiene permisos para realizar esta acción
-        
+
         if (!user) {
             throw new Error('Unauthorized');
         }
 
-        const { id = UUID(), ...rest } = form;
+        const { id = UUID(), imageFiles, ...rest } = form;
         rest.slug = rest.slug.toLowerCase().replace(' ', '-').trim();
 
         const product = {
@@ -46,6 +63,9 @@ export const createUpdateProduct = defineAction({
         } else {
             await db.update(Product).set(product).where(eq(Product.id, id));
         }
+
+        // Imagenes
+        console.log({imageFiles});
 
         return product;
     }
