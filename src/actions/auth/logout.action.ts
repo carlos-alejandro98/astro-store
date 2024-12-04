7872b8e0740618dc7f8cdc6ea2de690d48d3a424
Carlos Abarca Calderon <extcabarca@transbank.cl>
2024-12-03 23:01:27 -0300

    correcciones

--- src/actions/auth/logout.action.ts
+++ src/actions/auth/logout.action.ts
@@ -2,7 +2,7 @@ import { defineAction } from 'astro:actions';
 
 export const logout = defineAction({
   accept: 'json',
-  handler: async (_, { cookies }) => {
+  handler: async () => {
     return { ok: true };
   },
 });
