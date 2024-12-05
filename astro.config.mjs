124a86cb0684e966cebfb3fd1473042d79f5a77f
Carlos Abarca Calderon <extcabarca@transbank.cl>
2024-12-04 21:41:10 -0300

    corrección login astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import db from "@astrojs/db";
-import auth from "auth-astro";
+import auth from 'auth-astro';
 
 import react from "@astrojs/react";
 
